8bdbdda69162a9806d8e65807f0c24d9c8672f76
Joel Tankard <joel.tankard@togetherai.com>
2023-11-09 08:33:09 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const dataCloseChar = {
+  "{": "}",
+  "[": "]",
+  '"': '"',
+};
+
+module.exports = (jsonString) => {
+  if (!jsonString) return null;
+
+  let string = jsonString
+    .trim()
+    .replace(/(\r\n|\n|\r|\s{2,})/gm, "")
+    .replace(/(?<=:)([a-zA-Z]+)(?=\s*(?![,\}])(?:[,\}\s]|$))/g, " null");
+
+  let missingChars = [];
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+    if (char === missingChars[missingChars.length - 1]) {
+      missingChars.pop();
+    } else if (dataCloseChar[char]) {
+      missingChars.push(dataCloseChar[char]);
+
+      if (char === "{") {
+        missingChars.push(":");
+      }
+    }
+  }
+  if (missingChars[missingChars.length - 1] === ":") {
+    if (string[string.length - 1] !== "{") {
+      missingChars[missingChars.length - 1] = ": null";
+    } else {
+      missingChars.pop();
+    }
+  }
+  const missingCharsString = missingChars.reverse().join("");
+  const completeString = string + missingCharsString;
+  const cleanedString = completeString
+    .replace(/"":/g, "")
+    .replace(/":}|": }/g, '": null }')
+    .replace(/,""}|,}|,\"\w+\"}/g, "}")
+    .replace(/},]/g, "}]");
+
+  return cleanedString;
+};
